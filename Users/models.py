4c0319beca52143900a003997e03af5e861274ea
cmkishores <cmkishores@gmail.com>
2019-11-29 20:22:11 +0530

    Basic Structure

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    CHOICES = [
+        ('candidate', 'Candidate'),
+        ('interviewer', 'Interviewer'),
+        ('hr','HR Manager'),
+    ]
