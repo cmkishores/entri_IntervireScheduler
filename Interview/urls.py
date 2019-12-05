8c71f792d3f29f4866a2a9892ccb851370c56711
cmkishores <cmkishores@gmail.com>
2019-12-05 11:14:34 +0530

    change name of url

--- Interview/urls.py
+++ Interview/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import InterviewScheduleView
 
 urlpatterns = [
-		path('submit/', InterviewScheduleView.as_view(),name='signup'),
+		path('submit/', InterviewScheduleView.as_view(),name='submit'),
 	]
