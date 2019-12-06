4544fb7c3f52203f41b1a3a29cebfb3cf42447fb
cmkishores <cmkishores@gmail.com>
2019-12-06 20:16:27 +0530

    add url for result

--- Interview/urls.py
+++ Interview/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import InterviewScheduleView,SchedulerView
+from .views import InterviewScheduleView,SchedulerView,ResultView
 
 urlpatterns = [
 		path('submit/', InterviewScheduleView.as_view(),name='submit'),
 		path('schedule/', SchedulerView.as_view(),name='schedule'),
+		path('result/', ResultView.as_view(),name='result'),
+
+		
 
 	]
